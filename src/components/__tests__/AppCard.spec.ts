c0dad6c0c9a7c634908b1fab2509a9f15032bf1f
andriytereschuk <shatskyat@gmail.com>
2023-01-06 08:08:41 +0100

    [lighthouse-action] test lighthouse action

--- src/components/__tests__/AppCard.spec.ts
+++ src/components/__tests__/AppCard.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import AppCard from '../AppCard.vue'
 
-describe('HelloWorld', () => {
+describe('AppCard', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(AppCard, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
